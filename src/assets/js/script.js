57b52f59e194b5ee244e3c645757cc02114b66a0
Sabareesh N V <sabareeshnv@gmail.com>
2022-11-22 23:43:39 +0530

    changed navbar to slide out style

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -5,9 +5,17 @@ const navbarToggle = () => {
   });
 };
 
+function toggleSlideOver() {
+  document.getElementById('slideover-container').classList.toggle('invisible');
+  document.getElementById('slideover-bg').classList.toggle('opacity-0');
+  document.getElementById('slideover-bg').classList.toggle('opacity-50');
+  document.getElementById('slideover').classList.toggle('translate-x-full');
+}
+
 // Used so we can add more than one function in this same js script
 const app = () => {
-  navbarToggle();
+  // navbarToggle();
+  toggleSlideOver();
 };
 
 app();
