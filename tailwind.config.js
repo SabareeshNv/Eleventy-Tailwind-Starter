7ea17bfd9c2a54b5fff1e5953094ceccf35eb644
Sabareesh N V <sabareeshnv@gmail.com>
2022-11-23 23:32:17 +0530

    fixed navigation and a bunch of small annoyances

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,16 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./src/**/*.{html,njk,md}"],
   theme: {
+    // To center a container
+    container: {
+      center: true,
+      padding: '1rem',
+    },
     extend: {
       fontFamily: {
-        'sans': ['Rubik', ...defaultTheme.fontFamily.sans]
+        // 'sans': ['Rubik', ...defaultTheme.fontFamily.sans]
+        'sans': ['Poppins', ...defaultTheme.fontFamily.sans],
+
       },
     },
   },
